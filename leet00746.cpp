7bd01aab53bb0ce3615f3456868a105ad5817f71
erdoganpak <erdoganpak@gmail.com>
2021-12-08 15:25:36 +0100

    Update leet00746.cpp

--- leet00746.cpp
+++ leet00746.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) 
-    {
-        vector<int> f(cost.size());
-        f[0] = cost[0];
-        f[1] = cost[1];
-        
+    {        
         if(cost.size() > 1)
         {        
             for(int i = 2; i < cost.size(); i++)
             {
-                f[i] = cost[i] + min(f[i-1] , f[i-2]);
+                cost[i] = cost[i] + min(cost[i-1] , cost[i-2]);
             }
         }
-        
-    
-        return min (f[cost.size()-1], f[cost.size()-2]);
+        return min (cost[cost.size()-1], cost[cost.size()-2]);
     }
 };
