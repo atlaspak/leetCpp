c05f586a1081283a0b43dfcefa2b08d23da46727
erdoganpak <erdoganpak@gmail.com>
2021-12-21 20:28:38 +0100

    Create leet00344.cpp

---
+++ leet00344.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) 
+    {
+        char temp;
+        for(int i = 0, j = s.size()-1; i < s.size() / 2; ++i, --j)
+        {
+            temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+        }
+    }
+};
