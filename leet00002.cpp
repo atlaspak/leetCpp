f9eab2e1f66f67d7e2ac8c76571de42acc7fbd7c
erdoganpak <erdoganpak@gmail.com>
2021-12-22 19:09:19 +0100

    Update leet00002.cpp

--- leet00002.cpp
+++ leet00002.cpp
@@ -1,27 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    std::vector<int> twoSum(std::vector<int>& nums, int target)
-    {
-        std::vector<std::vector<int>> pairs;
-        for(int i = 0; i<nums.size(); i++)
-        {
-            std::vector<int> pair;
-            
-            if(pairs.size() != 0)
-            {
-                for(int j = 0; j<pairs.size(); j++)
-                {
-                    if(nums.at(pairs.at(j).at(0)) + nums.at(i) == target)
-                    {
-                        pairs.at(j).push_back(i);
-                        return pairs.at(j);
-                    }
-                }
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* head = new ListNode();
+        ListNode* current = head;
+        int l1Val, l2Val, leftOver = 0, currVal = 0;
+        while(true){                
+            if(l1){
+                l1Val = l1->val;
+                l1 = l1->next;
             }
-            pair.push_back(i);
-            pairs.push_back(pair);
+            else
+                l1Val = 0;
+            
+            if(l2){
+                l2Val = l2->val;
+                l2 = l2->next;
+            }
+            else
+                l2Val = 0;
+            
+            currVal = l1Val + l2Val + leftOver;
+            leftOver = 0;
+            if(currVal > 9){
+                leftOver = 1;
+                currVal -= 10;
+            }
+            current->val = currVal;
+            
+            if(!l1 && !l2){
+                if(leftOver > 0){
+                    current->next = new ListNode(leftOver);
+                }
+                break;
+            }
+            current->next = new ListNode();
+            current = current->next;
         }
-        std::vector<int> pair(2, 0);
-        return pair;
+        
+        return head;
     }
 };
